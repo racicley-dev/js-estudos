4e9e1e35f64db0d14736adc9a1945ab32468b2ef
Racicley <racicley@gmail.com>
2022-06-08 00:33:21 -0300

    Ajustes na formatação do texto.

--- Atividade 06 - Consumo de API/scripts.js
+++ Atividade 06 - Consumo de API/scripts.js
@@ -45,17 +45,14 @@ const loadImg = async() =>{
     obj = ar[0];
     sim = ar[1];
 
-    //console.log(sim.data);
     let custo_final = obj.mana_cost;
     let efeito_final = obj.oracle_text;
     for(let item of sim.data){
         if(custo_final.indexOf(item['symbol']) !== -1){
-            //console.log(item['svg_uri']);
-            //console.log(item['symbol']);
             let nome_img = '<img src=\"'+item['svg_uri']+'\" id=\"custo_c\">';
         
             custo_final = custo_final.replaceAll(item['symbol'], nome_img);
-            console.log(custo_final);
+
         }
 
         if(efeito_final.indexOf(item['symbol']) !== -1){
@@ -63,17 +60,17 @@ const loadImg = async() =>{
             let nome_efeito = '<img src=\"'+item['svg_uri']+'\" id=\"custo_c\">';
         
             efeito_final = efeito_final.replaceAll(item['symbol'], nome_efeito);
+
         }
     }
-    //console.log(custo_final);
+
 
     img.src = obj.image_uris.large;
     nome.innerHTML ="Nome da carta: "+ obj.name;
     custo_mana.innerHTML ="Custo de mana: "+ custo_final;
-    efeito.innerHTML ='Efeito da Carta: '+ efeito_final;
+    efeito.innerHTML ='Efeito da Carta: <br/>'+ efeito_final.replaceAll('\n', '<br/>');
     tipo.innerHTML = 'Tipo da carta: '+ obj.type_line;
 
-    //console.log(obj);
 }
 
 btn[0].addEventListener('click', loadImg);
